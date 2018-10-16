68fad93ffd2ec54a247d82e20f4189fadcce6387
rfist <theseuship@gmail.com>
2018-10-16 23:23:35 +0300

    add mongodb driver

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 require('dotenv').load();
 const fs = require('fs');
 const path = require('path');
+
+const db = require('./app/controllers/mongo');
 const controller = require('./app/components/bot');
 const logger = require('./app/logger');
-
-require('./app/components/express_webserver.js')(controller);
-
-const normalizedPath = path.join(__dirname, 'app', 'skills');
-fs.readdirSync(normalizedPath).forEach((file) => {
-  require(`./app/skills/${file}`)(controller);
-});
+const webserver = require('./app/components/express_webserver.js');
 
 logger.info('Starting...');
+
+db.connectToServer()
+  .then(() => webserver.start(controller))
+  .then(() => {
+    const normalizedPath = path.join(__dirname, 'app', 'skills');
+    fs.readdirSync(normalizedPath).forEach((file) => {
+      require(`./app/skills/${file}`)(controller);
+      logger.info(`Connect ${file} skill`);
+    });
+    logger.info('Bot has been started');
+  });
